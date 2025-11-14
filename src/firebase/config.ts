5f6eea1b2dcc35353a48b841c29970f8d4f2567d
tian tao <taotian021@gmail.com>
2025-11-14 13:50:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8273468999-d46b8",
+  "appId": "1:456631461288:web:8fff61e0d34ac21430e99d",
+  "apiKey": "AIzaSyC-rwNXAS3XsWxRNz076-9If9HuGxB0lRc",
+  "authDomain": "studio-8273468999-d46b8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "456631461288"
+};
