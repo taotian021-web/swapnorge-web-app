c167c8f64fb1c8ef3e629d44f6315602ec01143f
tian tao <taotian021@gmail.com>
2025-11-14 13:01:51 +0000

    2

---
+++ e2e/example.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/NeighborBuy/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('/');
+
+  // Expects the page to have a heading with the name of What's new in your neighborhood?.
+  await expect(page.getByRole('heading', { name: 'What\'s new in your neighborhood?' })).toBeVisible();
+});
